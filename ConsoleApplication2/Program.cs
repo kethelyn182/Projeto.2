fadd82c70e3514cdb6272f3b9dfe7d0cdaa3a674
kethelyn <kethelyn.jesus@etec.sp.gov.br>
2022-03-23 22:30:16 -0300

    gravacao projeto

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,6 +10,45 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            int num1 = 0;
+            int num2 = 0;
+            string continuar = "s";
+            Console.WriteLine("Calculadora em C#");
+
+            do
+            {
+
+                Console.WriteLine("Digite o primeiro número");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segundo número");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escolha as operações listadas abaixo: ");
+                Console.WriteLine(" A - Adição");
+                Console.WriteLine(" S - Subtração");
+                Console.WriteLine(" M - Multiplicação");
+                Console.WriteLine(" D - Divisão");
+                Console.WriteLine("Qual a sua opção?");
+
+                switch (Console.ReadLine())
+                {
+                    case "A":
+                        Console.WriteLine("Seu resultado: {num1} + {num2} = " + (num1 + num2)); break;
+                    case "S":
+                        Console.WriteLine("Seu resultado: {num1} - {num2} = " + (num1 - num2)); break;
+                    case "M":
+                        Console.WriteLine("Seu resultado: {num1} * {num2} = " + (num1 * num2)); break;
+                    case "D":
+                        Console.WriteLine("Seu resultado: {num1} / {num2} = " + (num1 / num2)); break;
+
+                }
+                Console.WriteLine("Pressione a tecla s para calcular novamente");
+                continuar = Console.ReadLine();
+               
+            } while ((continuar == "s" || continuar == "S"));
+               }
+            }
         }
-    }
-}
+
+    
+
